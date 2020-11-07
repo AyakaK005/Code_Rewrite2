4cb629f493d5adfe867b8189ff23af3ee58d6ee9
Ayaka <akawano2005@gmail.com>
2020-11-07 10:44:11 -0800

    Added Commands
    Added Commands for Drive

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,24 +1,23 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.subsystems;
+package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.nerdherd.lib.oi.DefaultOI;
 
-public class ExampleSubsystem extends SubsystemBase {
-  /**
-   * Creates a new ExampleSubsystem.
-   */
-  public ExampleSubsystem() {
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+/**
+ * Add your docs here.
+ */
+public class OI extends DefaultOI{
+    public JoystickButton ForwardVoltage;
+    public OI(){
+        super();
+        ForwardVoltage = new JoystickButton(driveJoyLeft, 1);
+    }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,39 +1,34 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-
 package frc.robot.subsystems;
 
-import com.nerdherd.lib.drivetrain.experimental.Drivetrain;
+import com.nerdherd.lib.drivetrain.experimental.ShiftingDrivetrain;
 import com.nerdherd.lib.motor.motorcontrollers.CANMotorController;
 import com.nerdherd.lib.motor.motorcontrollers.NerdyFalcon;
+import com.nerdherd.lib.pneumatics.Piston;
 import com.nerdherd.robot.RobotMap;
 
 // import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Drive extends Drivetrain {
+public class Drivetrain extends ShiftingDrivetrain {
   /**
-   * Creates a new Drivetrain.
+   * Creates a new ShiftingDrivetrain.
    */
-  private static final double trackwidth = 0.0;
-  public Drive() {
-    super(new NerdyFalcon(RobotMap.kLeftParentTalonID), new NerdyFalcon(RobotMap.kRightParentTalonID),
-        new CANMotorController[] { new NerdyFalcon(
-            RobotMap.kLeftChildTalonID) },
-        new CANMotorController[] { new NerdyFalcon(
-            RobotMap.kRightChildTalonID) },
-    true,
-    false,
-    Drive.trackwidth
-    );
-  }
-
-  public void setPower(double leftPow, double rightPow) {
-    super.setPower(leftPow, rightPow);
+  public Drivetrain() {
+    super(new NerdyFalcon(RobotMap.kLeftParentTalonID),
+    new NerdyFalcon(RobotMap.kRightParentTalonID),
+    new CANMotorController[] {
+      new NerdyFalcon(RobotMap.kLeftChildTalonID),
+    },
+    new CANMotorController[] {
+      new NerdyFalcon(RobotMap.kRightChildTalonID),
+    },
+    true, false, new Piston(RobotMap.kShifterPort1, RobotMap.kShifterPort2), RobotMap.kTrackWidth );
   }
 
   @Override
